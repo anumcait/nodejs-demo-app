5b3d6313656eb38733e182c17838b7728020a631
anumcait <53853412+anumcait@users.noreply.github.com>
2025-09-23 22:35:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello from CI/CD pipeline 🚀');
+// Serve static files from 'public' folder
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
